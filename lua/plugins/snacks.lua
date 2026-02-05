059ea62f0aaf5befb363d820d65bb561b3a1fcad
hudaierbas <hudaierbas@gmail.com>
2026-02-05 11:24:43 +0300

    update snacks.nvim

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      explorer = {
+        hidden = false,
+        ignored = false,
+      },
+      picker = {
+        hidden = true,
+        ignored = false,
+        sources = {
+          files = {
+            hidden = true,
+            ignored = false,
+          },
+        },
+      },
+    },
+  },
+}
